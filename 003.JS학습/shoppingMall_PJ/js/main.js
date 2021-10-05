48a1b7c6706eb1cb87a675355130a4aae7f2f44f
Tom B. Parker <69783504+tombap8@users.noreply.github.com>
2021-10-05 14:59:18 +0900

    Update main.js

--- 003.JS학습/shoppingMall_PJ/js/main.js
+++ 003.JS학습/shoppingMall_PJ/js/main.js
@@ -8,7 +8,25 @@ window.addEventListener("load",()=>{
     // 로딩완료확인
     console.log("로딩완료!");
 
-    
+    // [ 구현내용 ]
+    // - 버튼 클릭시 배너를 다음/이전으로 이동함
+    // - 이벤트 대상: .abtn(이동버튼2개)
+    let abtn = document.querySelectorAll(".abtn");
+    console.log("버튼개수:"+abtn.length);
+    // - 변경대상: #slide
+    let slide = document.querySelector("#slide");
+
+    ///// 오른쪽 버튼 클릭시 ///////////
+    // 내용: 슬라이드가 오른쪽으로 이동하여 다음슬라이드가 보임!
+    abtn[1].onclick = ()=>{
+
+        // 1. 호출확인
+        console.log("오른쪽이양!");
+
+    }; //////////// click함수 ////////////
+
+
+
 
 
 });//////////// load구역 ///////////////////
