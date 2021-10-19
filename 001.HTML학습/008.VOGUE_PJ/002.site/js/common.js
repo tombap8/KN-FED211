45b4b472b7f0d1a9c37eb144d246d50bc5407fdc
Tom B. Parker <69783504+tombap8@users.noreply.github.com>
2021-10-19 12:09:21 +0900

    SS

--- 001.HTML학습/008.VOGUE_PJ/002.site/js/common.js
+++ 001.HTML학습/008.VOGUE_PJ/002.site/js/common.js
@@ -1,5 +1,13 @@
 // 보그 코리아 공통 기능 JS - common.js //
-$(function(){ ////// jQB ////////////////////////
+
+/// 모바일여부 구분 상태변수
+let mob = 0; // 1-모바일, 0-모바일아님
+/// 처음에 화면가로사이즈로 상태값 업데이트함!
+if ($(window).width() <= 500) mob = 1;
+// 화면가로 사이즈가 500px 이하이면 mob값을 1으로 변경!
+
+
+$(function () { ////// jQB ////////////////////////
     // 페이지 새로 고침시 스크롤이 중간에 있으면
     // 브라우저 스크롤위치 캐싱(즉, 위치기억)에
     // 의해서 그 위치에 있게 되므로
@@ -24,58 +32,66 @@ $(function(){ ////// jQB ////////////////////////
     // 스크롤위치변수
     let scTop;
     // 스크롤실행상태변수(상단영역)
-    let scSts = 1;// 실행전1, 실행후0
+    let scSts = 1; // 실행전1, 실행후0
     // 스크롤실행상태변수2(탑버튼)
-    let scSts2 = 1;// 실행전1, 실행후0
+    let scSts2 = 1; // 실행전1, 실행후0
     // 상단영역요소
     let tm = $("#top");
     // 위로이동버튼 요소
     let tbtn = $(".tbtn");
-    
+
 
     // 전체 스크롤의 대상은 화면이므로 window객체를 사용한다!
-    $(window).scroll(function(){
+    $(window).scroll(function () {
 
         // 1. 전체 스크롤 위치값 알아내기
         // (1) scrollTop() 메서드 : 세로스크롤 위치값
         // (2) scrollLeft() 메서드 : 가로스크롤 위치값
 
         scTop = $(this).scrollTop();
-        console.log("스위:"+scTop);
+        console.log("스위:" + scTop);
 
-        // 1. 상단영역(#top)이 스크롤위치값 100px이상일때
+        // 1. 상단이동버튼 보이기 숨기기 ///
+        // 대상: .tbtn -> tbtn변수에 할당!
+        // 기준위치: 300px 스크롤위치
+        if (scTop >= 300 && scSts2 === 1) {
+            scSts2 = 0; //한번만!
+            console.log("탑버튼보여!");
+            tbtn.fadeIn(300); //쓱~보여
+        } ///////// if ///////////////////
+        else if (scTop < 300 && scSts2 === 0) {
+            scSts2 = 1; //한번만
+            console.log("탑버튼숨겨!");
+            tbtn.fadeOut(300); //쓱~숨겨
+        } ///////// else if //////////////
+
+
+        // 모바일일때 돌아가! ////////
+        if (mob) return;
+        // mob===1 일때 true처리된다!
+        // 아랫쪽 코드가 실행안됨!
+        // 위에 있는 top버튼 보이기/숨기기는 실행됨!
+        // 모바일에서도!
+
+        // 2. 상단영역(#top)이 스크롤위치값 100px이상일때
         // -> #top에 .on을 넣어서 상단영역 디자인 변경하기
         // 대상: #top -> 변수 tm에 할당!
-        if(scTop >= 100 && scSts===1){ // 100이상
-            scSts = 0;//한번밖에 못들어옴!
+        if (scTop >= 100 && scSts === 1) { // 100이상
+            scSts = 0; //한번밖에 못들어옴!
             console.log("실행1");
             tm.addClass("on");
             // 아래코드로 공간보정해봐유~!
-            $(".cont").css("margin-top","160px");
+            $(".cont").css("margin-top", "160px");
         } ////////// if //////////
-        else if(scTop < 100 && scSts===0){ // 100미만
-            scSts = 1;//한번밖에 못들어옴!
+        else if (scTop < 100 && scSts === 0) { // 100미만
+            scSts = 1; //한번밖에 못들어옴!
             console.log("실행2");
             tm.removeClass("on");
             // 아래코드로 공간보정해봐유~!
-            $(".cont").css("margin-top","0px");
+            $(".cont").css("margin-top", "0px");
         } //////// else /////////////
 
 
-
-        // 2. 상단이동버튼 보이기 숨기기 ///
-        // 대상: .tbtn -> tbtn변수에 할당!
-        // 기준위치: 300px 스크롤위치
-        if(scTop >= 300 && scSts2===1){
-            scSts2 = 0;//한번만!
-            console.log("탑버튼보여!");
-            tbtn.fadeIn(300);//쓱~보여
-        } ///////// if ///////////////////
-        else if(scTop < 300 && scSts2===0){
-            scSts2 = 1;//한번만
-            console.log("탑버튼숨겨!");
-            tbtn.fadeOut(300);//쓱~숨겨
-        } ///////// else if //////////////
 
 
     }); //////////// scroll //////////////////////
@@ -83,7 +99,7 @@ $(function(){ ////// jQB ////////////////////////
 
     /// 탑버튼 이동 구현하기 /////
     // 대상: .tbtn === tbtn변수
-    tbtn.click(function(e){ // e-이벤트전달변수
+    tbtn.click(function (e) { // e-이벤트전달변수
         // 기본이동막기
         e.preventDefault();
 
@@ -91,7 +107,7 @@ $(function(){ ////// jQB ////////////////////////
         // 변경대상: html,body 두가지모두!
         $("html,body").animate({
             scrollTop: "0px"
-        }, 1000,"easeInOutQuad");
+        }, 1000, "easeInOutQuad");
     }); ///////// click ////////////
 
 
