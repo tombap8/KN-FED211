c16186dabab26204ba0b7a211536785616eef4e1
Tom B. Parker <69783504+tombap8@users.noreply.github.com>
2021-10-25 14:02:08 +0900

    Create index.php

---
+++ 009.Ajax학습/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP 첫페이지</title>
+    <!-- PHP코딩영역 -->
+    <?php 
+        //주석
+        /* 주석 */
+        # 주석
+        // 화면출력 -> echo 메시지 
+        echo "<h1>나의 첫 PHP페이지!</h1>";
+        
+    ?>
+</head>
+<body>
+    
+</body>
+</html>
